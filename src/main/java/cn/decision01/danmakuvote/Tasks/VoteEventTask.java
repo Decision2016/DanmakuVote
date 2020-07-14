c94596222d5e34ef3d5347a301fc4d4421cb355d
decision2016 <787362202@qq.com>
2020-07-14 21:27:14 +0800

    Edit config.yml
    具体随机/定时投票开始过程需要进行细化，需要考虑swith指令输入时线程数问题

---
+++ src/main/java/cn/decision01/danmakuvote/Tasks/VoteEventTask.java
@@ -0,0 +1,21 @@
+package cn.decision01.danmakuvote.Tasks;
+
+import cn.decision01.danmakuvote.DanmakuVote;
+import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class VoteEventTask extends BukkitRunnable {
+    private final DanmakuVote plugin;
+
+    public VoteEventTask(DanmakuVote plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        if (! plugin.getSwitchStatus()) return ;
+        BukkitTask task = new DanmakuListenTask(plugin).runTaskAsynchronously(plugin);
+        Bukkit.getLogger().info("Event async thread started.");
+    }
+}
